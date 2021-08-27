a9bd3cdc30831fc6abe52c369b25e9ba339b7973
Arun-KumarH <arun.kumar@n-fuse.co>
2021-08-27 14:02:09 +0200

    Delete createConfigMap.bash

--- createConfigMap.bash
+++
@@ -1,9 +0,0 @@
-CM_STATUS="$(kubectl get cm -n=xingular | grep invoicing-srv-config | wc -l)"
-
-if [ "$CM_STATUS" -eq "1" ]; then
-   echo "delete \"invoicing-srv-config\" configmap..."
-   kubectl delete cm invoicing-srv-config -n=xingular
-fi
-
-echo "create \"invoicing-srv-config\" configmap..."
-kubectl create cm invoicing-srv-config -n=xingular --from-file=./cfg/
